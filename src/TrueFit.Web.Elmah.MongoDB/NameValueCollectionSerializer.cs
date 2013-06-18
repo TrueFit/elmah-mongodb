9eb6666a3d0a77c75988d6d19ac8bc1f5802271e
Adam Kirkton <akirkton@truefitinnovation.com>
2013-06-18 17:24:54 -0400

    Rename projects, update build configuration and pull in fix from upstream source repo for serialization

---
+++ src/TrueFit.Web.Elmah.MongoDB/NameValueCollectionSerializer.cs
@@ -0,0 +1,79 @@
+﻿#region usings
+
+using System;
+using System.Collections.Specialized;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
+#endregion
+
+namespace TruFit.Web.Elmah.MongoDB
+{
+  public class NameValueCollectionSerializer : BsonBaseSerializer
+  {
+    private static readonly NameValueCollectionSerializer instance = new NameValueCollectionSerializer();
+
+    public static NameValueCollectionSerializer Instance
+    {
+      get { return instance; }
+    }
+
+    public override object Deserialize(BsonReader bsonReader, Type nominalType, Type actualType, IBsonSerializationOptions options)
+    {
+      return Deserialize(bsonReader, nominalType, options);
+    }
+
+    public override object Deserialize(BsonReader bsonReader, Type nominalType, IBsonSerializationOptions options)
+    {
+      var bsonType = bsonReader.GetCurrentBsonType();
+      if (bsonType == BsonType.Null)
+      {
+        bsonReader.ReadNull();
+        return null;
+      }
+
+      var serializer = new StringSerializer();
+      var nvc = new NameValueCollection();
+
+      bsonReader.ReadStartArray();
+      while (bsonReader.ReadBsonType() != BsonType.EndOfDocument)
+      {
+        bsonReader.ReadStartArray();
+        var key = (string) serializer.Deserialize(bsonReader, typeof (string), options);
+        var val = (string) serializer.Deserialize(bsonReader, typeof (string), options);
+        bsonReader.ReadEndArray();
+        nvc.Add(key, val);
+      }
+      bsonReader.ReadEndArray();
+
+      return nvc;
+    }
+
+    public override void Serialize(BsonWriter bsonWriter, Type nominalType, object value, IBsonSerializationOptions options)
+    {
+      if (value == null)
+      {
+        bsonWriter.WriteNull();
+        return;
+      }
+
+      var serializer = new StringSerializer();
+      var nvc = (NameValueCollection) value;
+
+      bsonWriter.WriteStartArray();
+      foreach (var key in nvc.AllKeys)
+      {
+        foreach (var val in nvc.GetValues(key))
+        {
+          bsonWriter.WriteStartArray();
+          serializer.Serialize(bsonWriter, typeof (string), key, options);
+          serializer.Serialize(bsonWriter, typeof (string), val, options);
+          bsonWriter.WriteEndArray();
+        }
+      }
+      bsonWriter.WriteEndArray();
+    }
+  }
+}
---
+++ src/TrueFit.Web.Elmah.MongoDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿#region usings
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("TrueFit.Web.Elmah.MongoDB")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("Simon Green and TrueFit, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2011 Simon Green and TrueFit, Inc. All Rights Reserved.")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
